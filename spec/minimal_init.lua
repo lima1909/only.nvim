25462897c864296269b1a266dd7c50770dd45e12
lima1909 <linke.mario@web.de>
2024-12-15 16:04:55 +0100

    add github ci pipeline

---
+++ spec/minimal_init.lua
@@ -0,0 +1,19 @@
+-- git checkout plenary
+local plenary_dir = "/tmp/plenary.nvim"
+if vim.fn.isdirectory(plenary_dir) == 0 then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/nvim-lua/plenary.nvim",
+		plenary_dir,
+	})
+end
+
+vim.opt.rtp:append(".")
+vim.opt.rtp:append(plenary_dir)
+
+vim.cmd.runtime({ "plugin/plenary.vim", bang = true })
+
+require("plenary.busted")
